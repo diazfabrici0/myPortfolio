1ef78a7e21887825ac48d86ff0fd7ca52173aa88
Fabricio <118219604+diazfabrici0@users.noreply.github.com>
2025-05-09 20:26:07 -0300

    slider listo

--- portfolio/src/app/components/cuerpo/cuerpo.component.ts
+++ portfolio/src/app/components/cuerpo/cuerpo.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SliderComponent } from '../slider/slider.component';
 
 @Component({
   selector: 'app-cuerpo',
-  imports: [],
+  imports: [CommonModule, SliderComponent],
   templateUrl: './cuerpo.component.html',
   styleUrl: './cuerpo.component.css'
 })
 export class CuerpoComponent {
+ 
 
 }
---
+++ portfolio/src/app/components/slider/slider.component.ts
@@ -0,0 +1,60 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  imports: [CommonModule],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.css'
+})
+export class SliderComponent {
+  images = [
+    'assets/image/angular.png',
+    'assets/image/bootstrap.png',
+    'assets/image/html.png',
+    'assets/image/css.png',
+    'assets/image/django.png',
+    'assets/image/github.png',
+    'assets/image/javascript.png',
+    'assets/image/jquery.png',
+    'assets/image/laravel.png',
+    'assets/image/mysql.png',
+    'assets/image/nodejs.png',
+    'assets/image/php.png',
+    'assets/image/react.png',
+    'assets/image/tailwind.png',
+    'assets/image/typescript.png',
+  ];
+
+  ngOnInit(){
+    setInterval(() => this.nextSlide(), 1500)
+  }
+
+  currentSlide = 0;
+
+  itemWidth = 358; // mismo valor que en CSS (max-w-[180px])
+
+  visibleCount = Math.floor(window.innerWidth / this.itemWidth); // valor por defecto
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.visibleCount = Math.floor(window.innerWidth / this.itemWidth);
+  }
+
+nextSlide() {
+  if (this.currentSlide < this.images.length - this.visibleCount) {
+    this.currentSlide++;
+  } else {
+    this.currentSlide = 0; // Reinicia al llegar al final
+  }
+}
+
+prevSlide() {
+  if (this.currentSlide > 0) {
+    this.currentSlide--;
+  } else {
+    this.currentSlide = this.images.length - this.visibleCount;
+  }
+}
+
+}
