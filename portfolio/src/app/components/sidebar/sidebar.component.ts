2f28e8781a5c409248cc658915d32caac3272f93
Fabricio <118219604+diazfabrici0@users.noreply.github.com>
2025-07-30 20:40:41 -0300

    haciendo el porfolio responsive

--- portfolio/src/app/components/sidebar/sidebar.component.ts
+++ portfolio/src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Host, HostListener, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faHouse, faUser, faCode, faFolderOpen, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faUser, faCode, faFolderOpen, faEnvelope, faBars } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
   
 @Component({
@@ -12,7 +12,27 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
   styleUrl: './sidebar.component.css',
   standalone: true
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+
+  menuAbierto = true;
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['event'])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    if (window.innerWidth >= 768){
+      this.menuAbierto = true;
+    } else {
+      this.menuAbierto = false;
+    }
+  }
+
   faHouse = faHouse;
   faUser = faUser;
   faCode = faCode;
@@ -20,4 +40,5 @@ export class SidebarComponent {
   faEnvelope = faEnvelope;
   faGithub = faGithub; 
   faLinkedin = faLinkedin;
+  faBars = faBars;
 }
